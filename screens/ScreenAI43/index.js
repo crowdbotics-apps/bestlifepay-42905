911385af554641100cfd88e0876ac4440c170e2c
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2023-09-08 23:51:04 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI43/index.js
+++ screens/ScreenAI43/index.js
@@ -1,13 +1,18 @@
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
 import React from 'react';
 import { SafeAreaView, StyleSheet, View, Text, Image, Button } from 'react-native';
 
 const ConfirmationScreen = () => {
+  const navigation = useNavigation();
   return <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <Image style={styles.image} source={{
         uri: 'https://tinyurl.com/42evm3m3'
       }} />
-        <Text style={styles.message}>Confirmation message</Text>
+        <Pressable onPress={() => {
+        navigation.navigate("ScreenAI27");
+      }}><Text style={styles.message}>Confirmation message</Text></Pressable>
         <Button title="Jump Back to My Home Page" onPress={() => console.log('Button pressed')} />
       </View>
     </SafeAreaView>;
